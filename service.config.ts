45f3c733b91c85c073af6e33a456514976552502
code-lucky <2924249993@qq.com>
2024-10-23 15:02:18 +0800

    first commit

---
+++ service.config.ts
@@ -0,0 +1,13 @@
+/** 不同请求服务的环境配置 */
+export const serviceConfig: Record<ServiceEnvType, Record<string, string>> = {
+  dev: {
+    // url: 'https://mock.apifox.cn/m1/4071143-0-default',
+    url: 'http://127.0.0.1:3002/backend',
+  },
+  test: {
+    url: 'https://mock.apifox.cn/m1/4071143-0-default',
+  },
+  prod: {
+    url: 'https://mock.apifox.cn/m1/4071143-0-default',
+  },
+}
